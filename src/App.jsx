836540d5ffbc3e0c824e596d70fe6fff09dc667d
Akarsh8933 <akarshkatiyarak123@gmail.com>
2024-07-11 21:08:24 +0530

    completed basic setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+
+
+const App = () => {
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default App
